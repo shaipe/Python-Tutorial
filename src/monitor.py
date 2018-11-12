99a9ab91accf29e54ef677d802e81c2230de584b
shaipe <shaipe@sina.com>
2018-11-12 18:06:23 +0800

    modify monitor py script

--- src/monitor.py
+++ src/monitor.py
@@ -219,6 +219,31 @@ class Logger(object):
             pass
 
 
+def get_file_encoding(file_path, def_encoding='utf-8'):
+
+    """
+    获取文件编码方式
+    :param file_path: 文件路径
+    :param def_encoding: 默认编码方式
+    :return:
+    """
+
+    import os
+
+    if os.path.exists(file_path):
+        with open(file_path, 'rb+') as f:
+            import chardet
+            encode = chardet.detect(f.read())
+            # print(encode)
+            enc = encode.get('encoding', def_encoding)
+            if enc.lower().startswith('utf-16'):
+                enc = def_encoding
+
+            return enc
+
+    return def_encoding
+
+
 class Monitor:
 
     def __init__(self):
@@ -233,66 +258,106 @@ class Monitor:
         """
         if self.cpu_count == 0:
             self.cpu_count = psutil.cpu_count()
-            print(self.cpu_count)
+            # print(self.cpu_count)
         return self.cpu_count
 
-    def monitor(self, proc_name: str, percent):
+    def monitor(self, proc_conf):
         """
         开始监控
-        :param proc_name: 监控进程名
-        :param percent: cpu使用率
+        :param proc_conf: 监控配置
         :return:
         """
 
-        processs = [p for p in psutil.process_iter(attrs=['pid', 'name', 'username']) if proc_name in p.info['name']]
+        processs = [p for p in psutil.process_iter(attrs=['pid', 'name', 'username'])
+                    if p.info['name'] in proc_conf.keys()]
+        # print(processs)
         for proc in processs:
+            proc_name = proc.name()
             try:
-                if proc_name.lower() == proc.name().lower():
+                # print(proc_name, proc_conf.keys(), proc_name in proc_conf.keys())
+                if proc_name in proc_conf.keys():
+                    percent = int(proc_conf[proc_name]["percent"])
+                    execute = proc_conf[proc_name]["execute"]
                     cpu_percent = proc.cpu_percent(interval=2) / self.get_cpus()
+                    # print(":::::", cpu_percent, percent, execute)
                     if cpu_percent >= percent:
-                        proc.kill()
-                        # pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
-                        self.logger.info('Process Name %s, CPU Use Percent %s UserName %s' %
-                                         (proc_name, cpu_percent, proc.info["username"]))
+                        if execute == "":
+                            proc.kill()
+                        else:
+                            if os.path.exists(execute):
+                                # print(execute)
+                                import subprocess
+                                subprocess.call(execute)
+
+                    self.logger.info('Process Name %s, CPU Use Percent %s UserName %s' %
+                                     (proc_name, cpu_percent, proc.info["username"]))
 
                 pass
             except psutil.NoSuchProcess:
                 pass
         pass
 
-    def start(self):
+    def get_config(self):
         """
-        采用循环的模式间隔5s检测一次
+        获取配置信息
         :return:
         """
         try:
             cf = configparser.ConfigParser()
             conf_path = os.path.join(os.getcwd(), 'monitor.ini')
+            # print(conf_path)
             if os.path.exists(conf_path):
-                cf.read(conf_path, encoding="utf-8-sig")  # 读取配置文件 encoding用于中文读取
+                encode = get_file_encoding(conf_path)
+                cf.read(conf_path, encoding=encode)  # 读取配置文件 encoding用于中文读取
             else:
-                # 增加section
-                cf.add_section('config')  # 配合write使用
-                # cf.write(open(conf_path, "w"))
+                with open(conf_path, 'w') as f:
+                    f.write("# [config]节点下配置监控时间间隔(秒),interval: 2\n")
+                    f.write("# 多节点需添加[process1...]\n# 添加监控进程信息\n#    process: 进程名\n"
+                            "#    percent: cpu使用率\n#    execute: cpu问个问题率超过后执行的文件\n\n")
+                    cf.add_section("config")
+                    cf.set("config", "interval", "2")
 
-                # 修改字段
-                cf.set("config", "process", "w3wp.exe")
-                cf.set("config", "percent", "60")
-                cf.set("config", "interval", "2")
-                cf.write(open(conf_path, "w"))
+                    # 增加section
+                    cf.add_section('process1')  # 配合write使用
+                    # 修改字段
+                    cf.set("process1", "process", "w3wp.exe")
+                    cf.set("process1", "percent", "60")
+                    cf.set("process1", "execute", "")
+                    cf.write(f)
 
-            process_name = cf.get("config", "process")
-            percent = int(cf.get("config", "percent"))
-            interval = int(cf.get("config", "interval"))
-
-            self.logger.info("Monitor CPU usage over %s percent every %s seconds to end the process %s" %
-                             (percent, interval, process_name))
-            while True:
-                time.sleep(interval)
-                self.monitor(process_name, percent)
-            pass
+            sections = cf.sections()
+            result = dict()
+            result["process"] = dict()
+            for section in sections:
+                if section == "config":
+                    result["interval"] = cf.get("config", "interval")
+                else:
+                    name = cf.get(section, "process").lower()
+                    data = {
+                        "percent": cf.get(section, "percent"),
+                        "execute": cf.get(section, "execute")
+                    }
+                    result["process"][name] = data
+            return result
         except Exception as ex:
+            raise ex
             self.logger.error(ex)
+
+    def start(self):
+        """
+        采用循环的模式间隔5s检测一次
+        :return:
+        """
+        conf = self.get_config()
+        # print(conf)
+        interval = int(conf.get("interval", 2))
+
+        self.logger.info("Monitor CPU usage Start")
+
+        while True:
+            time.sleep(interval)
+            self.monitor(conf["process"])
+        pass
 
 
 if __name__ == "__main__":
